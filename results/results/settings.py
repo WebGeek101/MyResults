e5736eaff96c8eecaba80213f5550b793b663b09
Shockedreally <pavanmanjunath768@gmail.com>
2022-07-19 12:49:16 +0530

    database to dbsqlite3

--- results/results/settings.py
+++ results/results/settings.py
@@ -79,17 +79,17 @@ WSGI_APPLICATION = 'results.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'results',
-            'USER': 'root',
-            'PASSWORD':'',
-            'HOST': 'localhost',
-            'PORT': '3306',
-            'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-            } 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+            # 'ENGINE': 'django.db.backends.mysql',
+            # 'NAME': 'results',
+            # 'USER': 'root',
+            # 'PASSWORD':'',
+            # 'HOST': 'localhost',
+            # 'PORT': '3306',
+            # 'OPTIONS': {
+            #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            # } 
     }
 }
 
